475babfdbbb9acc328792d25ce4445192aa74ed7
Emerson Solano <emerson-solano@hotmail.com>
2023-03-05 12:02:09 -0600

    Destructuring de objetos

--- js-for-react/index.js
+++ js-for-react/index.js
@@ -4,18 +4,18 @@
 function hello() {
   console.log("Hola desde JS");
 }
-hello();
+hello(); //resultado: Hola desde JS
 
 function sumar(x, y) {
   return x + y;
 }
-console.log(sumar(4, 5));
+console.log(sumar(4, 5)); //resultado: 9
 
 // parametros por defecto
 function restar(x, y = 0) {
   return x - y;
 }
-console.log(restar(4));
+console.log(restar(4)); //resultado: 4 + 0 = 4
 
 // objetos - properties and methods
 const user = {
@@ -55,5 +55,37 @@ title.innerText = 'JS';
 const button = document.createElement("button");
 button.innerText = 'Click';
 
-document.body.append(title);
+// agragamos un evento de escucha al boton para realizar una accion al dar clic
+button.addEventListener("click", function () {
+  console.log("Dio click al boton!");
+  title.innerText = 'JavaScript';
+  alert('Texto del titulo actualizado');
+})
+
+// agregar elementos al cuerpo de html
+document.body.append(title); 
 document.body.append(button);
+
+// Destructuring
+// al trabajar con funciones podemos recibir objetos, y estos objetos tambien pueden ser utilizados dentro del DOM
+const user2 = {
+  name: 'Yomal',
+  age: 30,
+}
+
+// function printInfo(user) {
+//   return '<h1> Hola ' + user.name + ' </h1>';
+// }
+// Estructurando
+// function printInfo({age}) {
+//   return '<h1> hola ' + age + ' </h1>';
+// }
+// Otra Forma de Estructurar
+function printInfo(user) {
+  const { name, age } = user;
+
+  return '<h1> User: ' + name + ' Age: ' + age + '</h1>'
+}
+console.log(printInfo(user2));
+
+document.body.innerHTML = printInfo(user2);
